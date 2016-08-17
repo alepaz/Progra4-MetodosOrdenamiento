e9694a75ca7ec1505982fe53b2bf90ecf0f912c1
Chrono <Chrono>
2016-08-16 19:15:42 -0600

    in forms method

---
+++ Ejemplos/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Ejemplos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lbxInput = new System.Windows.Forms.ListBox();
+            this.lbxOutput = new System.Windows.Forms.ListBox();
+            this.lblInputs = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(12, 118);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(130, 56);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar Dato";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // lbxInput
+            // 
+            this.lbxInput.FormattingEnabled = true;
+            this.lbxInput.ItemHeight = 16;
+            this.lbxInput.Location = new System.Drawing.Point(160, 69);
+            this.lbxInput.Name = "lbxInput";
+            this.lbxInput.Size = new System.Drawing.Size(120, 180);
+            this.lbxInput.TabIndex = 1;
+            // 
+            // lbxOutput
+            // 
+            this.lbxOutput.FormattingEnabled = true;
+            this.lbxOutput.ItemHeight = 16;
+            this.lbxOutput.Location = new System.Drawing.Point(300, 69);
+            this.lbxOutput.Name = "lbxOutput";
+            this.lbxOutput.Size = new System.Drawing.Size(120, 180);
+            this.lbxOutput.TabIndex = 2;
+            // 
+            // lblInputs
+            // 
+            this.lblInputs.AutoSize = true;
+            this.lblInputs.Location = new System.Drawing.Point(187, 38);
+            this.lblInputs.Name = "lblInputs";
+            this.lblInputs.Size = new System.Drawing.Size(65, 17);
+            this.lblInputs.TabIndex = 3;
+            this.lblInputs.Text = "Entradas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(331, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Salidas";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 73);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(130, 22);
+            this.txtNum.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Digite un número";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ordenar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 56);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Limpiar datos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 330);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInputs);
+            this.Controls.Add(this.lbxOutput);
+            this.Controls.Add(this.lbxInput);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Método de ShellSort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.ListBox lbxInput;
+        private System.Windows.Forms.ListBox lbxOutput;
+        private System.Windows.Forms.Label lblInputs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ejemplos/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplos
+{
+    public partial class Form1 : Form
+    {
+        //Bandera publica si todos los datos estan correctos
+        private bool bandera = true;
+        private int Num1;
+        private int contador = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            bandera = true;
+
+            //Validamos y obtenemos los valores
+            Num1 = obtener_valor(txtNum);
+
+            if (bandera)
+            {
+                lbxInput.Items.Add(Num1);
+
+            }
+
+            limpiar_input();
+
+            txtNum.Focus();
+        }
+
+        public void limpiar_input()
+        { //Para limpiar el textbox llamado "Pantalla"
+
+            txtNum.Text = "";
+
+        }
+
+        public int obtener_valor(TextBox t1)
+        { //Para transformar lo que se digite en el textbox a formato
+            //numerico
+            int valor = 0;
+
+            //Si el control esta vacio
+            if (t1.Text.Length > 0)
+            {
+                //Si el control tiene caracteres
+                if (int.TryParse(t1.Text, out valor))
+                {
+                    
+                        valor = Convert.ToInt16(t1.Text);
+                }
+                else
+                {
+                    bandera = false;
+                    MessageBox.Show("El valor debe ser numerico");
+                }
+
+            }
+            else
+            {
+                bandera = false;
+                MessageBox.Show("El control se encuentra vacio");
+            }
+
+            return valor;
+
+        }
+
+        public static void QuickSort(int[] nums, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = nums[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (nums[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (nums[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    //Cambio o swap informatico
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+            //Llamadas recursivas
+            if (left < j)
+            {
+                QuickSort(nums, left, j);
+            }
+
+            if (i < right)
+            {
+                QuickSort(nums, i, right);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lbxInput.Items.Clear();
+            lbxOutput.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbxOutput.Items.Clear();
+            contador = lbxInput.Items.Count;
+            MessageBox.Show(contador.ToString());
+            int i;
+            int[]  arreglo = new int[contador];
+
+            for (i = 0; i < contador; i++) {
+
+                arreglo[i] = int.Parse(lbxInput.Items[i].ToString());
+
+            }
+
+            QuickSort(arreglo, 0, contador - 1);
+
+            for (i = 0; i < contador; i++) {
+                lbxOutput.Items.Add(arreglo[i]);
+            }
+        }
+    }
+}
